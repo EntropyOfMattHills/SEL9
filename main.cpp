1a3192df517c1031b587da76cbbf21a772bd4af2
EntropyOfMattHills <matthewhillnedalz@gmail.com>
2020-06-15 00:39:00 +0400

    Added mult of 2 digits

--- main.cpp
+++ main.cpp
@@ -8,5 +8,6 @@ int main(){
     int first, second;
     cout<<"Enter two digit numbers:"<<endl;
     cin>>first>>second;
-    cout<<"The sum is "<<first-second<<endl;
+    cout<<"The sum is "<<first - second<<endl;
+    cout<<"The mult is "<<first * second<<endl;
 }
